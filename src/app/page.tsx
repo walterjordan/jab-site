88378a91bcef475c016af109b041ee63506542dd
walterjordan <walterjordan@jordanborden.com>
2025-10-16 16:07:04 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
-"use client";
-import { useEffect, useState } from "react";
+// src/components/MessengerAccessWizard.jsx
+'use client';
+
+import React, { useEffect, useMemo, useState } from 'react';
 
 function DemoCarousel() {
   const slides = [
