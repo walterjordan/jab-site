7da8086544cbcbf017a1f39c06ef2333353471fa
walterjordan <walterjordan@jordanborden.com>
2026-03-23 15:17:31 -0400

    fix: resolve TypeScript window.gtag type error

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -1,5 +1,11 @@
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
 
+declare global {
+  interface Window {
+    gtag: (...args: any[]) => void;
+  }
+}
+
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: string) => {
   if (typeof window !== "undefined" && GA_TRACKING_ID) {
